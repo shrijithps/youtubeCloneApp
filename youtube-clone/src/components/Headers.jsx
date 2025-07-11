dc0f3920ede4986e1eceaaec6f5772bba9aae083
shrijithps <p.shrijith@talview.com>
2025-07-11 23:39:54 +0530

    fix

---
+++ youtube-clone/src/components/Headers.jsx
@@ -0,0 +1,7 @@
+export default function Headers() {
+    return (
+        <header>
+            <h1>Youtube Clone</h1>
+        </header>
+    );
+}
---
+++ youtube-clone/src/pages/VideoDetail.jsx
@@ -0,0 +1,7 @@
+export default function VideoDetail() {
+    return (
+        <header>
+            <h1>Youtube Clone</h1>
+        </header>
+    );
+}
