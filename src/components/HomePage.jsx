c3d5ad4e4d966d10c597351ac5482efaf632de30
shrijithps <p.shrijith@talview.com>
2025-07-12 18:19:48 +0530

    added_structure

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,10 @@
+import VideoCard from "./VideoCard";
+
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <VideoCard />
+        </div>
+    );
+}
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,3 +1,42 @@
-export default function VideoCard() {
-    return <div>VideoCard</div>;
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+export default function VideoCard({video}) {
+    const{
+        id,
+        title,
+        thumbnail,
+        channelTitle,
+        channelAvatar,
+        views,
+        publishedAt,
+    } = video
+
+    return (
+        <Link to={`/watch/${id}`}>
+            <div className="">
+                <img
+                    src={thumbnail}
+                    alt={title}
+                    className=""
+                />
+
+            <div className="">
+                <img
+                    src={channelAvatar}
+                    alt={channelTitle}
+                    className=""
+                />
+             </div>
+
+             <div className="">
+                 <h2>{title}</h2>
+                 <p>{channelTitle}</p>
+                 <p>{views} * {publishedAt}</p>
+             </div>
+
+
+            </div>
+        </Link>
+    )
 }
