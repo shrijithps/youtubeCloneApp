8573c300af65b5ccfdccd3c985745c24fcf87f95
shrijithps <p.shrijith@talview.com>
2025-07-18 17:13:24 +0530

    feat(ae):sidebar for clone

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,50 @@
+import React, {useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faHouse } from '@fortawesome/free-solid-svg-icons';
+import {youtubeShorts, subscription} from '../index';
+
 function SideBar() {
+
+    const [show, setShow] = useState(false);
+
     return (
         <div>
-            <h1>Youtube Clone</h1>
+            <FontAwesomeIcon icon={faBars} size="2xl"
+                className="fixed top-6 left-4"
+                onClick={()=> {
+                    setShow(!show);
+                    }
+                }
+            />
+
+            { show && (
+               <ul>
+                <li className="Home">
+                    <FontAwesomeIcon icon={faHouse}/>
+                    Home
+                </li>
+
+                <li
+                    className="">
+                    <img
+                        src={youtubeShorts}
+                        alt=""
+                        className="w-10 h-10"
+                    />
+                    Shorts
+                </li>
+
+                <li
+                    className="">
+                    <img
+                        src={subscription}
+                        alt=""
+                        />
+                >Subscriptions</li>
+                <li>Music</li>
+               </ul>
+            )}
+
         </div>
     );
 }
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import subscription from './assets/subscription.svg';
+import youtubeShorts from './assets/youtubeShorts.svg';
+import youtubemusic from './assets/youtubemusic.svg'
+
+
+export {subscription, youtubeShorts, youtubemusic}
